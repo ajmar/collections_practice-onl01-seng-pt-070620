22eb0761572cc066935bfc46185517261d5f5619
Alexander Marusin <amarusin18@gmail.com>
2020-07-17 23:10:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
 
+
+def sort_array_asc(int)
+  int.sort
+end
+
+def sort_array_desc(int)
+  int.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    else a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(str)
+  str.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    else a.length < b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(int)
+int.reverse
+end
+
+def kesha_maker(array)
+  array.each {|str| str[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(arr)
+  while arr[0, 1] || arr[3..20]
+   arr.map {|word| word + "s"}
+ end
+end
